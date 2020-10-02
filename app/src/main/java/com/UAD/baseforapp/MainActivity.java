5bec123c9e855b395cc7253752f9a3258a750521
RMX999 <aljithchonedan999@gmail.com>
2020-10-02 10:51:17 +0530

    second commit

--- app/src/main/java/com/UAD/baseforapp/MainActivity.java
+++ app/src/main/java/com/UAD/baseforapp/MainActivity.java
@@ -11,22 +11,7 @@ public class MainActivity extends AppCompatActivity
 {  @Override 
 protected void onCreate(Bundle savedInstanceState) {  super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-	}
-	@Override  public boolean onCreateOptionsMenu(Menu menu) 
-	{  getMenuInflater().inflate(R.menu.menu_main, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-	@Override  
-	public boolean onOptionsItemSelected(MenuItem item) {  switch (item.getItemId()) {  case R.id.action_create_order:
-				Intent intent = new Intent(this, orderactivity.class);
-				startActivity(intent);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
 		
-		}
 	
 	
 	}
